0b916cc124c711d3a399c65640f053b2ded13340
axelvuillaume <axelvuillaume@hotmail.fr>
2023-05-22 16:06:51 +0200

    login page v1

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
     import "iapau-components/iapau-card";
     import "iapau-components/iapau-textarea";
+    import "iapau-components/iapau-button";
+    import "iapau-components/iapau-password";
 </script>
 
+
+
 <div class="column-center" style="height: 100%">
-    <iapau-card class="column-center" style="height: fit-content">
-        <h1> Connectez vous </h1>
-        <iapau-textarea placeholder ="E-mail"></iapau-textarea>
-        <iapau-textarea placeholder ="Mot de pase"></iapau-textarea>
+    <iapau-card class="column-center card">
+        <img src="img/iapau_round.png" alt="logo"  style="width: 35%"/>
+        <p style="font-size: 1.5vw"> Connectez vous </p>
+        <iapau-textarea placeholder="E-mail"  style="margin-top:5%" cols="35">></iapau-textarea>
+        <iapau-password style="margin-top:5%"></iapau-password>
+        <iapau-button mode="primary"  hoverColors="true" style="margin-top:5%">Connexion</iapau-button>
+        <iapau-button mode="secondary" hoverColors="true" style="margin-top:5%" >Inscription</iapau-button>
     </iapau-card>
 
 </div>
@@ -15,6 +22,10 @@
 
 <style>
 
-
+    .card{
+        height: fit-content;
+        width: fit-content;
+        padding: 2% 20%;
+    }
 
 </style>

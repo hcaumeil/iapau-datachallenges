3941ca7c2f0ab665e150d52169f4c473c818d2b4
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2023-05-22 10:41:15 +0200

    add font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,28 +1,43 @@
 <script lang="ts">
-    import "iapau-components/css/default-theme.css"
+  import "iapau-components/css/default-theme.css";
 </script>
 
+<slot />
 
 <style global>
-    :global(html) {
-        height: 100%;
-        margin: 0;
-    }
+  :global(html) {
+    height: 100%;
+    margin: 0;
+  }
 
-    :global(body) {
-        height: 100%;
-        margin: 0;
-        font-family: "Montserrat";
-    }
+  :global(body) {
+    height: 100%;
+    margin: 0;
+    font-family: "Montserrat";
+  }
 
-    :global(.column-center) {
-        display: flex;
-        flex-direction: column;
-        justify-content: center;
-        align-items: center;
-        height: 100%;
-    }
+  :global(.column-center) {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+  }
+
+  @font-face {
+    font-family: "Montserrat";
+    src: url("/fonts/Montserrat-Medium.ttf");
+  }
+
+  @font-face {
+    font-family: "Montserrat";
+    src: url("/fonts/Montserrat-Light.ttf");
+    font-weight: 300;
+  }
+
+  @font-face {
+    font-family: "Montserrat";
+    src: url("/fonts/Montserrat-Bold.ttf");
+    font-weight: bold;
+  }
 </style>
-
-
-<slot/>

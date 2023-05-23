17be056d7b47f33a334d4ca32e0da1aaefc21e4b
axelvuillaume <axelvuillaume@hotmail.fr>
2023-05-23 13:18:14 +0200

    add inscription

---
+++ src/routes/inscription/+page.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+    import "iapau-components/iapau-card";
+    import "iapau-components/iapau-input";
+    import "iapau-components/iapau-button";
+    import "iapau-components/iapau-select";
+
+
+</script>
+
+
+
+<div class="column-center" style="height: 100%">
+    <iapau-card class="column-center card">
+        <p style="font-size: 1.5vw"> Inscription </p>
+
+        <form class="column-center card">
+        <iapau-input typeInput="text" placeholder="E-mail"  style="margin:5%"></iapau-input>
+        <iapau-input typeInput="password" placeholder="password"  style="margin:5%"></iapau-input>
+        <iapau-input typeInput="text" placeholder="Pseudo"  style="margin:5%"></iapau-input>
+        <iapau-select id="mySelect" style="margin:5%"></iapau-select>
+        <iapau-input typeInput="text" placeholder="Ecole"  style="margin:5%"></iapau-input>
+        </form>
+
+        <iapau-button mode="primary"  hoverColors="true" style="margin:5%">Continuer</iapau-button>
+    </iapau-card>
+
+
+
+
+
+
+    <script>
+        const selectElement = document.querySelector('#mySelect');
+        const options = ["Bac+1", "Bac+2", "Bac+3", "Bac+4", "Bac+5"];
+        selectElement.options = options;
+    </script>
+
+
+</div>
+
+
+
+<style>
+
+    .card{
+        height: fit-content;
+        width: fit-content;
+        padding: 2% 15%;
+    }
+
+</style>

08c3dcc93419637495011dde9e6691202bf1d3c9
axelvuillaume <axelvuillaume@hotmail.fr>
2023-05-23 10:37:07 +0200

    fix

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
     import "iapau-components/iapau-card";
-    import "iapau-components/iapau-textarea";
+    import "iapau-components/iapau-input";
     import "iapau-components/iapau-button";
-    import "iapau-components/iapau-password";
 </script>
 
 
 
 <div class="column-center" style="height: 100%">
     <iapau-card class="column-center card">
-        <img src="img/iapau_round.png" alt="logo"  style="width: 35%"/>
+        <img src="img/iapau_round.png" alt="logo"  style="width: 30%"/>
         <p style="font-size: 1.5vw"> Connectez vous </p>
-        <iapau-textarea placeholder="E-mail"  style="margin-top:5%" cols="35">></iapau-textarea>
-        <iapau-password style="margin-top:5%"></iapau-password>
-        <iapau-button mode="primary"  hoverColors="true" style="margin-top:5%">Connexion</iapau-button>
-        <iapau-button mode="secondary" hoverColors="true" style="margin-top:5%" >Inscription</iapau-button>
+        <iapau-input type="text" placeholder="E-mail"  style="margin-top:5%"></iapau-input>
+        <iapau-input type="password" style="margin-top:5%"></iapau-input>
+
+
+        <div style="display: flex; margin-top:5%">
+        <iapau-button mode="primary"  hoverColors="true" style="margin:5%">Connexion</iapau-button>
+        <iapau-button mode="secondary" hoverColors="true" style="margin:5%" >Inscription</iapau-button>
+        </div>
     </iapau-card>
 
 </div>
@@ -25,7 +28,7 @@
     .card{
         height: fit-content;
         width: fit-content;
-        padding: 2% 20%;
+        padding: 2% 15%;
     }
 
 </style>

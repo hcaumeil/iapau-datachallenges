d07b7d8a56864a25ef3d553320fb8c13060110a1
MatisToniutti <47952310+MatisToniutti@users.noreply.github.com>
2023-05-25 10:22:08 +0200

    fuse post and get folders

--- src/routes/api/users/+server.ts
+++ src/routes/api/users/+server.ts
@@ -16,9 +16,9 @@ const dbConfig = {
   password: PG_PASSWORD,
   port: parseInt(PG_PORT),
 };
+const client = new Client(dbConfig);
 
 export const GET: RequestHandler = async () => {
-  const client = new Client(dbConfig);
   await client.connect();
 
 try {
@@ -36,3 +36,29 @@ try {
       await client.end();
     }
 };
+
+export const POST: RequestHandler = async ({ request }) => {
+  try {
+    await client.connect();
+    const { email,surname,name,password,study_level,town,school,role,id_team } = await request.json();
+
+    const result = await client.query("INSERT INTO users (email,surname,name,password,salt,level,study_level,town,school,role) VALUES('"+email+"','"+surname+"','"+name+"','"+password+"','sel',0,'"+study_level+"','"+town+"','"+school+"','"+role+"');");
+
+    if(result.rowCount>0){
+      return new Response(JSON.stringify({
+        message: "Utilisateur créé",
+      }));
+    }
+    throw error(401, {
+      message: 'Informations not valid'
+    });
+    
+  } catch (error) {
+    console.error('Error fetching user table:', error);
+    return  new Response(JSON.stringify({
+        error: 'Internal Server Error',
+      }))
+  } finally {
+    await client.end();
+  }
+}
--- src/routes/api/users/[slug]/+server.ts
+++ src/routes/api/users/[slug]/+server.ts
@@ -1,5 +1,7 @@
 import { Client } from 'pg';
 import {PG_USER,PG_HOST,PG_DATABASE,PG_PASSWORD,PG_PORT} from '$env/static/private';
+import type { RequestHandler } from '@sveltejs/kit';
+import { error } from '@sveltejs/kit';
 
 const dbConfig = {
     user: PG_USER,
@@ -21,6 +23,39 @@ export async function GET({params}:any) {
     return new Response(JSON.stringify({
         users: users,
       }));
+  } catch (error) {
+    console.error('Error fetching user table:', error);
+    return  new Response(JSON.stringify({
+        error: 'Internal Server Error',
+      }))
+  } finally {
+    await client.end();
+  }
+}
+
+export const POST: RequestHandler = async ({ request,params }) => {
+  try {
+    await client.connect();
+    
+    const data = await request.json();
+    let query = "UPDATE users Set ";
+    Object.entries(data).forEach(([key, value]) => {
+        query = query.concat(key+"='"+value+"',");
+    });
+    query = query.slice(0, -1);
+    query = query.concat("WHERE id = '"+params.slug+"';" );
+    console.log(query);
+    const result = await client.query(query);
+
+    if(result.rowCount>0){
+      return new Response(JSON.stringify({
+        message: "Utilisateur modifié",
+      }));
+    }
+    throw error(401, {
+      message: 'Informations not valid'
+    });
+    
   } catch (error) {
     console.error('Error fetching user table:', error);
     return  new Response(JSON.stringify({

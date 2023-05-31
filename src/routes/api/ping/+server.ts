5480a0553786d416a98f118b13fdc72e177e15f0
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2023-05-31 15:21:33 +0200

    cutting api

--- src/routes/api/ping/+server.ts
+++
@@ -1,9 +0,0 @@
-//import type { RequestHandler } from "@sveltejs/kit";
-export const GET = ({ })=>{
-    // const ping = request.headers.get('Ping');
-    // if(ping==="ping"){
-
-    // }
-    return new Response(JSON.stringify('pong'));
-
-}

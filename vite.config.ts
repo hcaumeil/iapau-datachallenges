164cca43f3a4710d0eae2dfc4873217674a1458b
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2023-05-31 18:20:33 +0200

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,28 @@
-import { defineConfig } from 'vite'
-import { sveltekit } from '@sveltejs/kit/vite'
-import { internalIpV4 } from 'internal-ip'
-import wasm from "vite-plugin-wasm";
-import topLevelAwait from "vite-plugin-top-level-await";
+import { defineConfig } from "vite";
+import { sveltekit } from "@sveltejs/kit/vite";
+import { internalIpV4 } from "internal-ip";
 
 // https://vitejs.dev/config/
 export default defineConfig(async () => {
-  const host = await internalIpV4()
+  const host = await internalIpV4();
 
   /** @type {import('vite').UserConfig} */
   const config = {
-    plugins: [wasm(),
-    topLevelAwait(),sveltekit()],
+    plugins: [ sveltekit()],
     test: {
-      include: ['src/**/*.{test,spec}.{js,ts}'],
+      include: ["src/**/*.{test,spec}.{js,ts}"],
     },
     server: {
-      host: '0.0.0.0', // listen on all addresses
+      host: "0.0.0.0", // listen on all addresses
       port: 5173,
       strictPort: true,
       hmr: {
-        protocol: 'ws',
+        protocol: "ws",
         host,
         port: 5183,
       },
     },
-  }
+  };
 
-  return config
-})
+  return config;
+});

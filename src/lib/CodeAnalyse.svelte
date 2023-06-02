d8110b858d66e0b0f14ccb33fa276212edaa5bf5
hcaumeil <78665596+hcaumeil@users.noreply.github.com>
2023-06-02 07:36:36 +0200

    fix

--- src/lib/CodeAnalyse.svelte
+++ src/lib/CodeAnalyse.svelte
@@ -1,59 +1,56 @@
 <script>
+  import { analitycs_api } from "../routes/const.js";
 
-    import {analitycs_api} from "../routes/const.js";
+  let code = "";
+  let res = "";
 
-    let code= ""
+  async function handleButton() {
+    const data = {
+      code: code,
+    };
 
-    async function handleButton(){
-        const data = {
-            code:code
-        }
+    code.split(" ").forEach(e => data[e] = e)
 
-        try {
-            const response = await fetch(analitycs_api +'/raw', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify(data)
-            });
+    try {
+      const response = await fetch(analitycs_api + "/raw", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
 
-            if (response.ok) {
-                console.log('Code ajouté avec succès');
-            } else {
-                console.error("Erreur lors de l'ajout du Code");
-            }
-        } catch (error) {
-            console.error('Erreur lors de la requête POST', error);
-        }
-
-
+      if (response.ok) {
+        console.log("Code ajouté avec succès");
+        res = await response.json()
+      } else {
+        console.error("Erreur lors de l'ajout du Code");
+      }
+    } catch (error) {
+      console.error("Erreur lors de la requête POST", error);
     }
-
-
+  }
 </script>
 
+<div style="height: 100%" class="column-center">
+  <h2>Analyse code</h2>
 
-<div style="height: 100%">
+  <iapau-textarea
+    cols="50"
+    rows="15"
+    oninput={(e) => (code = e)}
+    style="margin: 5rem;"
+  />
 
-    <h2> Analyse code</h2>
+  <iapau-button
+    on:click={() => {
+      handleButton();
+    }}>Valider</iapau-button
+  >
 
-    <iapau-textarea cols="200" rows="50" oninput={(e) => (code = e)}>
-
-    </iapau-textarea>
-
-    <iapau-button on:click={() => {
-                handleButton();
-              }} >Valider</iapau-button>
-
-
-
-
+  <p>{res}</p>
+  <p />
 </div>
 
-
-
 <style>
-
-
 </style>
--- src/routes/const.js
+++ src/routes/const.js
@@ -1,3 +1,2 @@
 export const iapau_api = "http://app-3d5b711a-92e6-434b-ae4c-3db252bcfd7c.cleverapps.io"
-
-export const analitycs_api = "http://app-38457a11-d628-48fc-b425-37ea5af4826e.cleverapps.io"
+export const analitycs_api = "http://localhost:8081"
